6cced1738f4c320984f645831c6d42f0f5e297b2
Walmyr Filho <limaesilva@qelp.com>
2019-06-10 21:11:04 +0200

    Simplify test file
    Also, add an empty line in the end of the file.

--- tests/specs/sample.spec.js
+++ tests/specs/sample.spec.js
@@ -7,50 +7,44 @@ const contextValidation = require("../contextValidator")
 
 const context = process.env.TEST_CONTEXT
 
-if (context !== "accessibility" && context !== "functional" && context !== "visual") {
-  console.log(`Invalid test mode: ${context}`)
-} else {
-  console.log(`Running tests in ${context} mode.`)
+const SAMPLE_APP = "Sample app"
+const SHOW_FILE_NAME_ON_UPLOAD_BUTTON = "show file name on upload button"
 
-  const SAMPLE_APP = "Sample app"
-  const SHOW_FILE_NAME_ON_UPLOAD_BUTTON = "show file name on upload button"
+describe(SAMPLE_APP, () => {
+  let sampleApp
 
-  describe(SAMPLE_APP, () => {
-    let sampleApp
-
-    beforeEach(() => {
-      sampleApp = new SampleApp()
-      browser.get("")
-    })
+  beforeEach(() => {
+    sampleApp = new SampleApp()
+    browser.get("")
+  })
     
-    describe("shorten/expand", () => {
-      const shortUrl = "https://foo.bar"
-      const expandedUrl = "https://foo.bar.baz.bah.boo"
+  describe("shorten/expand", () => {
+    const shortUrl = "https://foo.bar"
+    const expandedUrl = "https://foo.bar.baz.bah.boo"
       
-      it("shortens a URL", () => {
-        sampleApp.shortenUrl(expandedUrl)
+    it("shortens a URL", () => {
+      sampleApp.shortenUrl(expandedUrl)
       
-        helper.waitForTextToBePresentInElement(sampleApp.result, shortUrl)
-      })
-      
-      it("expands a URL", () => {
-        sampleApp.expandUrl(shortUrl)
-      
-        helper.waitForTextToBePresentInElement(sampleApp.result, expandedUrl)
-      })
+      helper.waitForTextToBePresentInElement(sampleApp.result, shortUrl)
     })
       
-    describe("file upload", () => {
-      it(SHOW_FILE_NAME_ON_UPLOAD_BUTTON, () => {
-        const CARTOON_DOT_GIF = "cartoon.gif"
-        const relativePathOfFileToUpload = `../assets/${CARTOON_DOT_GIF}`
-        const absolutePathOfFileToUpload = path.resolve(__dirname, relativePathOfFileToUpload)
-  
-        helper.uploadFileIntoInputField(sampleApp.fileInputField, absolutePathOfFileToUpload)
-            
-        helper.waitForTextToBePresentInElement(sampleApp.fileUploadForm, CARTOON_DOT_GIF)
-        contextValidation(context, browser, SAMPLE_APP, SHOW_FILE_NAME_ON_UPLOAD_BUTTON)
-      })
+    it("expands a URL", () => {
+      sampleApp.expandUrl(shortUrl)
+      
+      helper.waitForTextToBePresentInElement(sampleApp.result, expandedUrl)
     })
   })
-}
+      
+  describe("file upload", () => {
+    it(SHOW_FILE_NAME_ON_UPLOAD_BUTTON, () => {
+      const CARTOON_DOT_GIF = "cartoon.gif"
+      const relativePathOfFileToUpload = `../assets/${CARTOON_DOT_GIF}`
+      const absolutePathOfFileToUpload = path.resolve(__dirname, relativePathOfFileToUpload)
+  
+      helper.uploadFileIntoInputField(sampleApp.fileInputField, absolutePathOfFileToUpload)
+            
+      helper.waitForTextToBePresentInElement(sampleApp.fileUploadForm, CARTOON_DOT_GIF)
+      contextValidation(context, browser, SAMPLE_APP, SHOW_FILE_NAME_ON_UPLOAD_BUTTON)
+    })
+  })
+})
